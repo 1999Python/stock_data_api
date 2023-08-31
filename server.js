16a99f0c51cd6bbcf6c5991c80876bf321b13fdd
1999Python <raadiyaharend6@gmail.com>
2023-08-31 21:04:31 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const cherrio = require('cheerio')
+const app = express()
+const port = 5353
+
+//variables
+const baseUrl = (stock) => `https://finance.yahoo.com/quote/${stock}/history${stock}`
+
+//middleware
+app.use(express.json())
+app.use(require('cors')())
+
+//routes
+app.get('/', (req, res) =>{
+res.status(200).send({ message : 'Thank you for trying our API'})
+})
+
+app.get('/api/stock', (req, res) =>{
+    const { stock } = req.query
+    if (!stock){
+        return res.sendStatus(403)
+    }
+
+    const stockDataURL = baseURL = stock
+})
+
+app.post('/test', (req, res) =>{
+    const body = req.body
+    const { message } = body
+    console.log('This is the message' + message)
+    res.sendStatus(200)
+})
+
+app.listen(port, () => console.log(`Server has started on port: ${port}`))
